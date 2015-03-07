f0998e857e9bffcaa048bdca5ba2569e8d8d0802
Railag <whenthegroundcavedin@gmail.com>
2015-03-07 14:39:06 +0300

    migrating to android studio

---
+++ app/src/main/java/com/rtweel/services/TweetReceiver.java
@@ -0,0 +1,77 @@
+package com.rtweel.services;
+
+import com.rtweel.activities.MainActivity;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.appcompat.R;
+import android.util.Log;
+
+public class TweetReceiver extends BroadcastReceiver {
+
+	public static final String BROADCAST_ACTION = "com.rtweel.services.TweetReceiver.BROADCAST_ACTION";
+	public static final String CANCEL_NOTIFICATION_ACTION = "com.rtweel.services.TweetReceiver.CANCEL_NOTIFICATION_ACTION";
+
+	public static final int ACTION_BROADCAST = 1;
+	public static final int ACTION_CANCEL = 2;
+	
+	public TweetReceiver() {
+	}
+
+	@Override
+	public void onReceive(Context context, Intent data) {
+		Log.i("DEBUG", "onReceive");
+		int action = 0;
+		String actionString = data.getAction();
+		if (actionString.equals(BROADCAST_ACTION)) {
+			action = ACTION_BROADCAST;
+		} else if (actionString.equals(CANCEL_NOTIFICATION_ACTION)) {
+			action = ACTION_CANCEL;
+		}
+
+		switch (action) {
+		case ACTION_BROADCAST: {
+			String message = data.getStringExtra(TweetService.MESSAGE);
+			String title = data.getStringExtra(TweetService.TITLE);
+			NotificationCompat.Builder builder = new NotificationCompat.Builder(
+					context).setSmallIcon(R.drawable.abc_ab_share_pack_holo_dark)
+					.setContentTitle(title).setContentText(message);
+			builder.setAutoCancel(true);
+			builder.setSound(RingtoneManager.getActualDefaultRingtoneUri(
+					context, RingtoneManager.TYPE_NOTIFICATION));
+			Intent resultIntent = new Intent(context, MainActivity.class);
+
+			PendingIntent resultPendingIntent = PendingIntent
+					.getActivity(context, 0, resultIntent,
+							PendingIntent.FLAG_UPDATE_CURRENT);
+
+			builder.setContentIntent(resultPendingIntent);
+			builder.setLights(Color.CYAN, 1000, 4000);
+			Intent intent = new Intent(CANCEL_NOTIFICATION_ACTION);
+			PendingIntent deleteIntent = PendingIntent.getBroadcast(context, 0,
+					intent, 0);
+			builder.setDeleteIntent(deleteIntent);
+
+			int mNotificationId = 1;
+			NotificationManager mNotifyMgr = (NotificationManager) context
+					.getSystemService(Context.NOTIFICATION_SERVICE);
+			Notification notification = builder.build();
+			mNotifyMgr.notify(mNotificationId, notification);
+			break;
+		}
+		case ACTION_CANCEL: {
+			Log.i("DEBUG", "Action 2");
+			TweetService.setNewTweets(0);
+			break;
+		}
+		}
+	}
+
+}
---
+++ app/src/main/java/com/rtweel/twitteroauth/ConstantValues.java
@@ -0,0 +1,12 @@
+package com.rtweel.twitteroauth;
+
+public class ConstantValues {
+	static String TWITTER_CONSUMER_KEY = "Your consumer key";
+	static String TWITTER_CONSUMER_SECRET = "Your secren consumer key";
+	public static final String TWITTER_CALLBACK_URL = "oauth://your host";
+
+	public static String URL_PARAMETER_TWITTER_OAUTH_VERIFIER = "oauth_verifier";
+	public static String PREFERENCE_TWITTER_OAUTH_TOKEN = "TWITTER_OAUTH_TOKEN";
+	public static String PREFERENCE_TWITTER_OAUTH_TOKEN_SECRET = "TWITTER_OAUTH_TOKEN_SECRET";
+	public static String PREFERENCE_TWITTER_IS_LOGGED_IN = "TWITTER_IS_LOGGED_IN";
+}
