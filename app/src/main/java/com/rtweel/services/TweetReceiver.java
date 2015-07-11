4e983eed8b66788c48be82eeab427bb0e163543b
Railag <whenthegroundcavedin@gmail.com>
2015-07-11 18:22:40 +0300

    pn updates, service updates

--- app/src/main/java/com/rtweel/services/TweetReceiver.java
+++ app/src/main/java/com/rtweel/services/TweetReceiver.java
@@ -1,7 +1,5 @@
 package com.rtweel.services;
 
-import com.rtweel.MainActivity;
-
 import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -9,11 +7,18 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.os.SystemClock;
+import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+
+import com.rtweel.MainActivity;
+import com.rtweel.fragments.SettingsFragment;
+
+import java.util.concurrent.TimeUnit;
 
 public class TweetReceiver extends BroadcastReceiver {
 
@@ -26,7 +31,6 @@ public class TweetReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent data) {
-        Log.i("DEBUG", "onReceive");
         String actionString = data.getAction();
 
         switch (actionString) {
@@ -34,7 +38,7 @@ public class TweetReceiver extends BroadcastReceiver {
             case BROADCAST_ACTION:
                 String message = data.getStringExtra(TweetService.MESSAGE);
                 String title = data.getStringExtra(TweetService.TITLE);
-
+                TweetService.PN type = (TweetService.PN) data.getSerializableExtra(TweetService.TYPE);
 
                 Intent resultIntent = new Intent(context, MainActivity.class);
                 PendingIntent resultPendingIntent = PendingIntent
@@ -47,13 +51,22 @@ public class TweetReceiver extends BroadcastReceiver {
                         intent, 0);
 
                 NotificationCompat.Builder builder = new NotificationCompat.Builder(
-                        context).setSmallIcon(com.rtweel.R.drawable.rtweel)
+                        context)
                         .setContentTitle(title).setContentText(message)
                         .setAutoCancel(true)
                         .setSound(RingtoneManager.getActualDefaultRingtoneUri(context, RingtoneManager.TYPE_NOTIFICATION))
                         .setContentIntent(resultPendingIntent)
                         .setLights(Color.CYAN, 1000, 4000)
                         .setDeleteIntent(deleteIntent);
+
+
+                // TODO different notification design for different types
+                switch (type) {
+                    case MESSAGE:
+                    case MENTION:
+                    case UPDATE:
+                        builder.setSmallIcon(com.rtweel.R.drawable.rtweel);
+                }
 
 
                 int mNotificationId = 1;
@@ -65,11 +78,19 @@ public class TweetReceiver extends BroadcastReceiver {
                 break;
 
             case CANCEL_NOTIFICATION_ACTION:
-                Log.i("DEBUG", "Action 2");
                 TweetService.setNewTweets(0);
                 break;
 
             case BOOT_WAKEUP_ACTION:
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+                boolean isAlreadySet = prefs.contains(SettingsFragment.PN_INTERVAL);
+                int intervalInMinutes;
+                if (isAlreadySet)
+                    intervalInMinutes = prefs.getInt(SettingsFragment.PN_INTERVAL, 4 * 60);
+                else
+                    intervalInMinutes = 60;
+
+                long intervalInMillis = TimeUnit.MINUTES.toMillis(intervalInMinutes);
                 Intent serviceIntent = new Intent(context, TweetService.class);
                 PendingIntent alarmIntent = PendingIntent.getService(context, 0,
                         serviceIntent, PendingIntent.FLAG_UPDATE_CURRENT);
@@ -77,8 +98,8 @@ public class TweetReceiver extends BroadcastReceiver {
                 alarmManager
                         .setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
                                 SystemClock.elapsedRealtime()
-                                        + AlarmManager.INTERVAL_HALF_HOUR,
-                                AlarmManager.INTERVAL_HALF_HOUR, alarmIntent);
+                                        + intervalInMillis,
+                                intervalInMillis, alarmIntent);
 
         }
 
--- app/src/main/java/com/rtweel/services/TweetService.java
+++ app/src/main/java/com/rtweel/services/TweetService.java
@@ -3,28 +3,42 @@ package com.rtweel.services;
 import android.app.IntentService;
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
-import android.util.Log;
+import android.preference.PreferenceManager;
 
-import com.rtweel.storage.Tweets;
+import com.rtweel.R;
+import com.rtweel.fragments.SettingsFragment;
 import com.rtweel.storage.App;
+import com.rtweel.storage.Tweets;
 import com.rtweel.timelines.HomeTimeline;
 import com.rtweel.timelines.Timeline;
-import com.rtweel.timelines.UserTimeline;
 
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import twitter4j.DirectMessage;
+import twitter4j.Paging;
+import twitter4j.ResponseList;
 import twitter4j.Status;
+import twitter4j.TwitterException;
 
 public class TweetService extends IntentService {
 
     public static final String MESSAGE = "message";
-
     public static final String TITLE = "title";
+    public static final String TYPE = "type";
 
     private static int sNewTweets = 0;
 
     private Timeline mTimeline;
+
+    public enum PN {
+        MESSAGE,
+        MENTION,
+        UPDATE
+    }
 
     public TweetService() {
         super("TweetService");
@@ -38,39 +52,78 @@ public class TweetService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent data) {
-        if (!App.isOnline(getApplicationContext())) {
+        if (!App.isOnline(getApplicationContext()))
             return;
+
+        loadFromDB();
+
+        Paging paging = new Paging();
+        paging.setCount(1);
+
+
+        //check for new direct message
+        try {
+            ResponseList<DirectMessage> messages = Tweets.getTwitter(this).getDirectMessages(paging);
+            if (messages != null && messages.size() > 0 && isNew(messages.get(0).getCreatedAt())) {
+                DirectMessage message = messages.get(0);
+                sendPN(PN.MESSAGE, message.getText(), getString(R.string.pn_dmessage_title));
+                return;
+            }
+        } catch (TwitterException e) {
+            e.printStackTrace();
         }
 
-        String message;
-        Log.i("DEBUG", "onHandleIntent");
-        loadFromDB();
+
+        //check for new mentions
+
+        try {
+            ResponseList<Status> mentions = Tweets.getTwitter(this).getMentionsTimeline(paging);
+            if (mentions != null && mentions.size() > 0 && isNew(mentions.get(0).getCreatedAt())) {
+                Status mention = mentions.get(0);
+                sendPN(PN.MENTION, mention.getText(), getString(R.string.pn_mention_title));
+                return;
+            }
+
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+
+
+        //check for new tweets
+
         try {
             List<twitter4j.Status> downloadedList = mTimeline.downloadTimeline(
                     Timeline.UP_TWEETS);
             mTimeline.updateTimelineUpDb(downloadedList);
 
             int size = downloadedList.size();
-            Log.i("DEBUG", "Size: " + String.valueOf(size));
             if (size > 0) {
                 sNewTweets += size;
 
-                message = "Tweets downloaded: " + sNewTweets;
-                String title = "Tweet checking";
+                String message = getString(R.string.pn_new_message) + sNewTweets;
+                String title = getString(R.string.pn_new_title);
 
-                Intent localIntent = new Intent(TweetReceiver.BROADCAST_ACTION)
-                        .putExtra(MESSAGE, message);
-                localIntent.putExtra(TITLE, title);
-                sendBroadcast(localIntent);
-
-            } else {
-                message = "No new tweets";
+                sendPN(PN.UPDATE, message, title);
             }
-            Log.i("DEBUG", message);
         } catch (NullPointerException e) {
-            e.printStackTrace();
-            Log.i("DEBUG", "NPE in service");
         }
+    }
+
+    private boolean isNew(Date createdAt) {
+        Date currentDate = new Date();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        int interval = prefs.getInt(SettingsFragment.PN_INTERVAL, 4 * 60);
+        long intervalInMillis = TimeUnit.MINUTES.toMillis(interval);
+        return ! (currentDate.getTime() - createdAt.getTime() > intervalInMillis);
+
+    }
+
+    private void sendPN(PN type, String message, String title) {
+        Intent localIntent = new Intent(TweetReceiver.BROADCAST_ACTION)
+                .putExtra(MESSAGE, message)
+                .putExtra(TITLE, title)
+                .putExtra(TYPE, type);
+        sendBroadcast(localIntent);
     }
 
     private void loadFromDB() {
